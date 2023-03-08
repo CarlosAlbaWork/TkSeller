73a05b7abe108f74e106bd22a5b05b6a788d01e5
Manuel Mollar <mm.github@mobelt.com>
2023-03-08 19:53:13 +0100

    script retocado con precios

--- contracts/tokenPalero.sol
+++ contracts/tokenPalero.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract ERC20Palero is ERC20 {
-  constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+contract MyToken is ERC20, ERC20Permit {
+  constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {
     _mint(msg.sender, 120000 * 10 ** decimals());
   }
 }
--- scripts/deploy_and_test.ts
+++ scripts/deploy_and_test.ts
@@ -3,6 +3,10 @@ import { BigNumber } from "hardhat"
 
 (async () => {
 
+  const hardcap= 30000
+
+  const dirETH = "0x0000000000000000000000000000000000000000"
+
   const decimals=18
   function sbgn(tk: BigNumber) { return ethers.utils.formatUnits(tk,decimals); }
   function bgn (tk: number) { return ethers.utils.parseUnits(tk.toString(),decimals) }
@@ -32,20 +36,24 @@ import { BigNumber } from "hardhat"
   if (err)
     throw new Error("Mete saldo");
 
-  const hardcap= 30000
-  const deplTkSeller = provider.getSigner(0) // deploy y owner general del contrato de compraventa
+  let isig=0
+  const deplTkSeller = provider.getSigner(isig++) // deploy y owner general del contrato de compraventa
   console.log('Irelevante Super-amo de TkSeller',await deplTkSeller.getAddress())
-  const deplTkEnVenta = provider.getSigner(1) // deploy y owner del token en venta, suministrará los TK
+  const deplTkEnVenta = provider.getSigner(isig++) // deploy y owner del token en venta, suministrará los TK
   const dOwnTkEnVenta = await deplTkEnVenta.getAddress()
   console.log('Owner Token Venta',dOwnTkEnVenta)
-  const deplTkPago = provider.getSigner(2) // deploy y owner del token de pago
-  const iniciador = provider.getSigner(3)  // tipo que inicia la venta, por no poner el creador, podría ser dOwnTkEnVenta
+  const deplTkPago1 = provider.getSigner(isig++) // deploy y owner del token de pago1
+  const deplTkPago2 = provider.getSigner(isig++) // deploy y owner del token de pago2
+  const iniciador = provider.getSigner(isig++)  // tipo que inicia la venta, por no poner el creador, podría ser dOwnTkEnVenta
   const dIniciador = await iniciador.getAddress()
   console.log('Iniciador', dIniciador)
-  const comprador = provider.getSigner(4) // comprador, debe tener token de pago
-  const dComprador =  await comprador.getAddress()
-  console.log('Comprador',dComprador)
-  const pagaETH = provider.getSigner(5) // este comprará con ETH
+  const comprador1 = provider.getSigner(isig++) // comprador, debe tener token de pago
+  const dComprador1 =  await comprador1.getAddress()
+  console.log('Comprador 1',dComprador1)
+  const comprador2 = provider.getSigner(isig++) // comprador, debe tener token de pago
+  const dComprador2 =  await comprador2.getAddress()
+  console.log('Comprador 2',dComprador2)
+  const pagaETH = provider.getSigner(isig++) // este comprará con ETH
   const dPagaETH = await pagaETH.getAddress()
   console.log('Comprador con ETH',dPagaETH)
 
@@ -65,22 +73,30 @@ import { BigNumber } from "hardhat"
               ': BAL', sbgn(await cOwnTkEnVenta.balanceOf(dOwnTkEnVenta)),
               'ALLOW:', sbgn(await cOwnTkEnVenta.allowance(dOwnTkEnVenta,dirTkSeller)))
 
-  const tokenPago = await ethers.getContractFactory('ERC20Palero',deplTkPago)
-  console.log('=> deploy tokenPago')
-  const cOwnTkPago = await tokenPago.deploy('PAGO','PAGO')
-  const dirTkPago = cOwnTkPago.address
+  const tokenPago1 = await ethers.getContractFactory('ERC20Palero',deplTkPago1)
+  console.log('=> deploy tokenPago 1')
+  const cOwnTkPago1 = await tokenPago1.deploy('PAGO','PAGO')
+  const dirTkPago1 = cOwnTkPago1.address
+  console.log('Dir token Pago 1',dirTkPago1)
+  const tokenPago2 = await ethers.getContractFactory('ERC20Palero',deplTkPago2)
+  console.log('=> deploy tokenPago 2')
+  const cOwnTkPago2 = await tokenPago2.deploy('PAGO','PAGO')
+  const dirTkPago2 = cOwnTkPago2.address
+  console.log('Dir token Pago 2',dirTkPago2)
 
   // TkSeller visto por el iniciador
   const cIniciador = await ethers.getContractAt('TkSeller',dirTkSeller,iniciador)
   console.log('=> initSale')
-  // ownTkEnVenta tiene el total de tokens a vender, autoriza al contrato a coger
+  // cOwnTkEnVenta tiene el total de tokens a vender, autoriza al contrato a coger
   await espera(cOwnTkEnVenta.approve(dirTkSeller,bgn(hardcap)))
   // normalmente el iniciador será el propietario del token, pero no tiene por qué, por eso está separado
-  await espera(cIniciador.initSale(dirTkEnVenta,dOwnTkEnVenta,
-                                    bgn(hardcap),bgn(hardcap),bgn(hardcap/3),
-                                    Math.round(Date.now()/1000)+24*3600,
-                                    bgn(0.1),10,
-                                    true,''))
+  await espera(cIniciador.initSale(
+    dirTkEnVenta,dOwnTkEnVenta,
+    bgn(hardcap),bgn(hardcap),bgn(hardcap/3),
+    Math.round(Date.now()/1000)+24*3600,  // 1 día después
+    [bgn(0.1),bgn(10),bgn(8)],
+    [dirETH,dirTkPago1,dirTkPago2],
+    true,''))
   console.log('BAL en owner:', sbgn(await cOwnTkEnVenta.balanceOf(dOwnTkEnVenta)),
               'BAL en venta:', sbgn(await cOwnTkEnVenta.balanceOf(dirTkSeller)))
 
@@ -94,25 +110,45 @@ import { BigNumber } from "hardhat"
       await espera(pagaETH.sendTransaction({ to: dirTkSeller, value: bgn(10) , data: dirTkEnVenta })) mal ... solo envia ether
       console.log('Pagado con ether y recibido',await cli1TkEnVenta.balanceOf(pagaETH.address))
   */
-  // TkSeller visto por el comprador
-  const cCompTkSeller = await ethers.getContractAt('TkSeller',dirTkSeller,comprador)
+  // TkSeller visto por el comprador1
+  const cCompTkSeller1 = await ethers.getContractAt('TkSeller',dirTkSeller,comprador1)
   // el comprador debe tener tokens de PAGO, se los transfiere el owner
-  espera(cOwnTkPago.transfer(dComprador,bgn(10000)))
+  espera(cOwnTkPago1.transfer(dComprador1,bgn(10000)))
   // token de pago visto por el comprador
-  const cCompPago = await ethers.getContractAt('ERC20Palero',dirTkPago,comprador)
-  console.log(await cOwnTkPago.name(),
-              ': BAL del comprador:', sbgn(await cCompPago.balanceOf(dComprador)))
+  const cCompPago1 = await ethers.getContractAt('ERC20Palero',dirTkPago1,comprador1)
+  console.log(await cOwnTkPago1.name(),
+              ': BAL del comprador 1:', sbgn(await cCompPago1.balanceOf(dComprador1)))
+  // TkSeller visto por el comprador2
+  const cCompTkSeller2 = await ethers.getContractAt('TkSeller',dirTkSeller,comprador2)
+  // el comprador debe tener tokens de PAGO, se los transfiere el owner
+  espera(cOwnTkPago2.transfer(dComprador2,bgn(10000)))
+  // token de pago visto por el comprador
+  const cCompPago2 = await ethers.getContractAt('ERC20Palero',dirTkPago2,comprador2)
+  console.log(await cOwnTkPago2.name(),
+              ': BAL del comprador 2:', sbgn(await cCompPago2.balanceOf(dComprador2)))
 
-  console.log('=> buy 100')
+  let buy=100
+  console.log('=> Comprador 1 buy',buy)
   // autoriza que TkSeller le coja la pasta
-  await espera(cCompPago.approve(dirTkSeller,bgn(100)))
-  console.log('ALLOW:', sbgn(await cCompPago.allowance(dComprador,dirTkSeller)))
+  await espera(cCompPago1.approve(dirTkSeller,bgn(buy)))
+  console.log('ALLOW:', sbgn(await cCompPago1.allowance(dComprador1,dirTkSeller)))
   // llama al contrato
-  await espera(cCompTkSeller.buyTokensByToken(dirTkEnVenta,bgn(100),dirTkPago,''))
+  await espera(cCompTkSeller1.buyTokensByToken(dirTkEnVenta,bgn(100),dirTkPago1,''))
 
   // token en venta visto por el comprador
-  const cCompTkEnVenta = await ethers.getContractAt('ERC20Palero',dirTkEnVenta,comprador)
-  console.log('Pagado con Token y recibido',await cCompTkEnVenta.balanceOf(dComprador))
+  const cCompTkEnVenta1 = await ethers.getContractAt('ERC20Palero',dirTkEnVenta,comprador1)
+  console.log('Pagado con Token y recibido',await cCompTkEnVenta1.balanceOf(dComprador1))
+  
+  buy = 200
+  console.log('=> Comprador 2 buy',buy)
+  // autoriza que TkSeller le coja la pasta
+  await espera(cCompPago2.approve(dirTkSeller,bgn(buy)))
+  console.log('ALLOW:', sbgn(await cCompPago2.allowance(dComprador2,dirTkSeller)))
+  // llama al contrato
+  await espera(cCompTkSeller2.buyTokensByToken(dirTkEnVenta,bgn(buy),dirTkPago2,''))
 
-
+  // token en venta visto por el comprador
+  const cCompTkEnVenta2 = await ethers.getContractAt('ERC20Palero',dirTkEnVenta,comprador2)
+  console.log('Pagado con Token y recibido',await cCompTkEnVenta2.balanceOf(dComprador2))
+  
 })()
